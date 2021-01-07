a1befa28b50c959db2e30fda6ecc34988713a6fe
Logesh.S <76871611+Everything-with-python@users.noreply.github.com>
2021-01-07 22:44:15 +0530

    Update Whatsapp & Facebook msg.py

--- Whatsapp & Facebook msg.py
+++ Whatsapp & Facebook msg.py
@@ -1,9 +1,9 @@
 import pywhatkit
 
-pywhatkit.sendwhatmsg("**********","Hey, good morning")     # type the number to whom u want to send message first double quotes, then type the message 
+pywhatkit.sendwhatmsg("**********","Hey, good morning")     # type the number to whom u want to send message first double quotes, then type the message in second double quotes
 
 
 import pyautogui
 
 pyautogui.typewritter("Hey babe, whats up...")      # type the message here 
-pyautogui.press("enter")        # you don't to press enter, it'll press automatically
+pyautogui.press("enter")                            # you don't to press enter, it'll press automatically
